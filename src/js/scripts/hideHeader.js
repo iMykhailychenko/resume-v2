6e9da259ce2d403abb4ec2a303af28e4385028eb
DL agency <dlagency@MacBook-Air-air13.local>
2020-03-04 21:44:35 +0200

    header fix

--- src/js/scripts/hideHeader.js
+++ src/js/scripts/hideHeader.js
@@ -3,10 +3,8 @@ const headerBlock = document.querySelector('.header');
 
 const hideHeader = e => {
     if(e.wheelDeltaY > 0) {
-        console.log('up');
         header.classList.remove('header__hidden');
     } else {
-        console.log('down');
         header.classList.add('header__hidden');
     }
 };
